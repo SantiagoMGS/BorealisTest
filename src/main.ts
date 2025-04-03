f68f6f6a9740378cfac2963f9787fa2340009209
Pedro Luis Maldonado Garcia <pmaldonado@sunvalleyinv.com>
2025-04-03 22:07:38 +0000

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
     SwaggerModule.setup('api/docs', app, document);
 
     // **Iniciar la aplicación**
-    const port = process.env.PORT || 443;
+    const port = process.env.PORT || 3000;
     await app.listen(port);
 
     logger.log(`🚀 Aplicación iniciada en http://localhost:${port}/api`);
