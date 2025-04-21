48f34948bb8df0ffdeebf47ccc6a755480e2d1a9
Santiago Martinez <smartinez@sunvalleyinv.com>
2025-04-21 15:47:43 -0500

    Actualización de `schema.prisma`: se añaden índices y restricciones de unicidad en los modelos `Supplier`, `CompanySupplier`, `Reception`, `ReceptionUnit`, `SubSample`, `Barrenado`, `AnalysisType` y `Analysis`. Se modifica la enumeración `ReceptionOrigin` para incluir nuevos tipos. Estos cambios mejoran la integridad de los datos y optimizan las consultas en la base de datos.

---
+++ prisma/migrations/20250421203940_/migration.sql
@@ -0,0 +1,153 @@
+-- CreateEnum
+CREATE TYPE "DocumentType" AS ENUM ('NIT', 'NIT_EXTERIOR', 'PASAPORTE', 'CEDULA_DE_CIUDADANIA', 'CEDULA_DE_EXTRANJERIA');
+
+-- CreateEnum
+CREATE TYPE "ReceptionType" AS ENUM ('DORE', 'MUESTRAS', 'MINERAL', 'CONCENTRADO');
+
+-- CreateEnum
+CREATE TYPE "ReceptionOrigin" AS ENUM ('CABEZA', 'COLA', 'SECADO', 'JOYERIA', 'SUBSISTENCIA', 'VETA_FUNDIDO', 'MINERAL', 'CONCENTRADO');
+
+-- CreateEnum
+CREATE TYPE "SubSampleType" AS ENUM ('MPL', 'MPR', 'MPA', 'CUSTODIA', 'BARRENADO');
+
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "documentType" "DocumentType" NOT NULL,
+    "documentNumber" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CompanySupplier" (
+    "companyId" UUID NOT NULL,
+    "supplierId" UUID NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "CompanySupplier_pkey" PRIMARY KEY ("companyId","supplierId")
+);
+
+-- CreateTable
+CREATE TABLE "Reception" (
+    "id" UUID NOT NULL,
+    "companyId" UUID NOT NULL,
+    "supplierId" UUID NOT NULL,
+    "receptionType" "ReceptionType" NOT NULL,
+    "receptionOrigin" "ReceptionOrigin" NOT NULL,
+    "receptionDate" TIMESTAMP(3) NOT NULL,
+    "batchNumber" TEXT,
+    "observation" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "Reception_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ReceptionUnit" (
+    "id" UUID NOT NULL,
+    "receptionId" UUID NOT NULL,
+    "recievedWeight" DECIMAL(65,30) NOT NULL,
+    "dryWeight" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "ReceptionUnit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SubSample" (
+    "id" UUID NOT NULL,
+    "receptionUnitId" UUID NOT NULL,
+    "subSampleType" "SubSampleType" NOT NULL,
+    "weight" DECIMAL(65,30) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "SubSample_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Barrenado" (
+    "id" UUID NOT NULL,
+    "receptionUnitId" UUID NOT NULL,
+    "totalWeight" DECIMAL(65,30) NOT NULL,
+    "balanceWeight" DECIMAL(65,30) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "Barrenado_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AnalysisType" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "resultSchema" JSONB NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "AnalysisType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Analysis" (
+    "id" UUID NOT NULL,
+    "analysisTypeId" UUID NOT NULL,
+    "subSampleId" UUID NOT NULL,
+    "resultValue" JSONB NOT NULL,
+    "analysisDate" TIMESTAMP(3) NOT NULL,
+    "analysisResult" JSONB NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" UUID,
+    "updatedBy" UUID,
+
+    CONSTRAINT "Analysis_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CompanySupplier" ADD CONSTRAINT "CompanySupplier_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompanySupplier" ADD CONSTRAINT "CompanySupplier_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "Supplier"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reception" ADD CONSTRAINT "Reception_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Reception" ADD CONSTRAINT "Reception_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "Supplier"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReceptionUnit" ADD CONSTRAINT "ReceptionUnit_receptionId_fkey" FOREIGN KEY ("receptionId") REFERENCES "Reception"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SubSample" ADD CONSTRAINT "SubSample_receptionUnitId_fkey" FOREIGN KEY ("receptionUnitId") REFERENCES "ReceptionUnit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Barrenado" ADD CONSTRAINT "Barrenado_receptionUnitId_fkey" FOREIGN KEY ("receptionUnitId") REFERENCES "ReceptionUnit"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Analysis" ADD CONSTRAINT "Analysis_analysisTypeId_fkey" FOREIGN KEY ("analysisTypeId") REFERENCES "AnalysisType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Analysis" ADD CONSTRAINT "Analysis_subSampleId_fkey" FOREIGN KEY ("subSampleId") REFERENCES "SubSample"("id") ON DELETE CASCADE ON UPDATE CASCADE;
