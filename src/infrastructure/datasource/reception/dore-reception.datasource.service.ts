999929a8818ba039a53cf0945dca5565c98fcac1
Anggy <aruiz@sunvalleyinv.com>
2025-04-28 15:57:23 -0500

    Se añade un campo `code` aleatorio en el objeto de recepción dentro del servicio de datos `DoreReceptionDataSourceService`, mejorando la identificación de las recepciones.

--- src/infrastructure/datasource/reception/dore-reception.datasource.service.ts
+++ src/infrastructure/datasource/reception/dore-reception.datasource.service.ts
@@ -147,6 +147,7 @@ export class DoreReceptionDataSourceService {
             receivedWeight: item.receivedWeight,
             observation: item.observation,
             statusId: receivedStatus.id,
+            code: Math.floor(Math.random() * 1000000),
           },
           include: {
             status: {
