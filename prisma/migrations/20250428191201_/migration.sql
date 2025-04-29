5e51e6c2bf8ada0823a20e622905d43370f4c452
Santiago Martinez <smartinez9626@gmail.com>
2025-04-28 21:08:54 -0500

    Se modifica la secuencia `dore_code_seq` para que inicie en 30000, ajustando el valor por defecto del campo `code` en la tabla `dore`. Esto mejora la gestión de códigos en el sistema de recepción.

--- prisma/migrations/20250428191201_/migration.sql
+++ prisma/migrations/20250428191201_/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-CREATE SEQUENCE dore_code_seq;
+CREATE SEQUENCE dore_code_seq START WITH 30000;
 ALTER TABLE "dore" ALTER COLUMN "code" SET DEFAULT nextval('dore_code_seq');
 ALTER SEQUENCE dore_code_seq OWNED BY "dore"."code";
