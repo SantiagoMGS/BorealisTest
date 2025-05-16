b9c248fca2c60d27291d687a736e367b3f793cae
Santiago Martinez <smartinez9626@gmail.com>
2025-05-15 19:43:08 -0500

    Se elimina el archivo de configuración de ESLint `.eslintrc.js` y se crea un nuevo archivo `eslint.config.js` con una configuración más moderna y modular. Se actualizan las dependencias de ESLint y se añaden nuevos plugins para mejorar la gestión de importaciones no utilizadas y la integración con Prettier. Además, se modifican las configuraciones de VSCode para habilitar ESLint y mejorar la experiencia de desarrollo.

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import unusedImports from 'eslint-plugin-unused-imports';
+import prettierPlugin from 'eslint-plugin-prettier';
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['eslint.config.js', 'node_modules', 'dist'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: process.cwd(),
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'unused-imports': unusedImports,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      eqeqeq: ['error', 'always'],
+      'no-console': 'warn',
+      'no-duplicate-imports': 'error',
+      'no-magic-numbers': [
+        'warn',
+        { ignore: [0, 1, -1], ignoreArrayIndexes: true },
+      ],
+      'no-shadow': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-return-await': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+];
